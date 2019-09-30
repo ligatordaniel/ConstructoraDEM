9150417393bf7f5c17a0ee9d7f5bd5aa17db45c5
ligatorDaniel <danielulloa256@gmail.com>
2019-09-30 17:51:45 -0400

    detalles 17.51 30-09-2019

--- enviar2.php
+++ enviar2.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'danielulloa256@gmail.com';
+$para = 'rafael.toro.ulloa@gmail.com';
 $asunto = 'Mensaje pagina DEM';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
